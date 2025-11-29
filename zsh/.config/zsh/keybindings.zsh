7a70342eac5422cf97d4cf407dff78a2f0bf2dd8
Orges Mihaj <contact@mihaj.org>
2025-11-29 11:51:40 -0500

    Add zsh keybindings

---
+++ zsh/.config/zsh/keybindings.zsh
@@ -0,0 +1,12 @@
+# ───  keybindings.zsh  ─────────────────────────────────────────────╯
+
+bindkey -e
+
+bindkey '^p' history-search-backward
+bindkey '^n' history-search-forward
+
+bindkey '^[b' backward-word
+bindkey '^[f' forward-word
+
+bindkey '^A'  beginning-of-line
+bindkey '^E'  end-of-line
