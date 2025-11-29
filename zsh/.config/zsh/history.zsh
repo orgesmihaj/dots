9fed6b5c7c420f1de8700ebb1e6206507d4b8abd
Orges Mihaj <contact@mihaj.org>
2025-11-29 17:09:31 -0500

    Configure the Zsh history

---
+++ zsh/.config/zsh/history.zsh
@@ -0,0 +1,18 @@
+# ───  history.zsh  ────────────────────────────────────────────────╯
+
+HISTSIZE=5000
+SAVEHIST=$HISTSIZE
+HISTFILE="${XDG_STATE_HOME:-$HOME/.local/state}/zsh/history"
+
+mkdir -p "${HISTFILE:h}"
+
+# ─── history persistence behavior ──────────────────────────────────
+
+setopt appendhistory        # Append, don't overwrite
+setopt inc_append_history   # Write each command immediately
+
+# ─── filtering / cleaning ──────────────────────────────────────────
+
+setopt hist_ignore_dups     # Ignore consecutive duplicates
+setopt hist_ignore_space    # Skip commands starting with space
+setopt hist_reduce_blanks   # Trim superfluous whitespace 
